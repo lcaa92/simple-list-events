48b33c0c78580ef824a8cdd4923478d37c8c2f09
Leandro Araujo <leandrocaaraujo@gmail.com>
2025-08-04 14:35:49 -0300

    Add DataService

---
+++ services/DataService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace services;
+
+/**
+ * Class DataService
+ *
+ * Handles loading and retrieving event data from a CSV file.
+ */
+class DataService
+{
+    /**
+     * @var array $events Array of events loaded from CSV.
+     */
+    protected $events = [];
+
+    /**
+     * DataService constructor.
+     * Loads events from the CSV file on instantiation.
+     */
+    function __construct() {
+        $this->loadEvents();
+    }
+
+    /**
+     * Loads events from the seeds.csv file into the $events property.
+     *
+     * @return void
+     */
+    protected function loadEvents()
+    {
+        $file = fopen('seeds.csv', 'r');
+        while (($line = fgetcsv($file)) !== FALSE) {
+            $this->events[] = [
+                'event_name'=> $line[0],
+                'location' => $line[1],
+                'address' => $line[2]
+            ];
+        }
+    }
+
+    /**
+     * Retrieves all events or a specific event by ID.
+     *
+     * @param int|null $id Optional event index.
+     * @return array|mixed Returns all events if $id is null, otherwise returns the event at the given index.
+     * @throws \OutOfBoundsException If the provided ID does not exist in the events array.
+     */
+    public function getEvents($id = null)
+    {
+        if(!isset($id)){
+            return $this->events;
+        }
+
+        return $this->events[$id];
+    }
+}
